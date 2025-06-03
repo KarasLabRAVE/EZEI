0990c04c3133b5c0564625d8538331a5c40f9d4d
CecileLes <139265906+CecileLes@users.noreply.github.com>
2025-06-03 17:23:12 -0500

    round time label to first decimal in energy ratio plot

--- R/visualization.R
+++ R/visualization.R
@@ -2,18 +2,18 @@
 makeHeatMap <- function(df, xTicksNum = 10){
   xLabels <- colnames(df)
   yLabels <- rownames(df)
-  
+
   if(is.null(xLabels)){
     xLabels <- seq_len(ncol(df))
   }
   if(is.null(yLabels)){
     yLabels <- seq_len(nrow(df))
   }
-  
+
   df$y <- yLabels
   df_long <- reshape2::melt(df, id.vars = "y", variable.name = "x", value.name = "value")
   colnames(df_long) <- c("y", "x", "value")
-  
+
   ## sort df_long by rownames(ERMatReorderd)
   df_long$x <- factor(df_long$x, levels = xLabels)
   df_long$y <- factor(df_long$y, levels = rev(yLabels))
@@ -25,7 +25,7 @@ makeHeatMap <- function(df, xTicksNum = 10){
   } else {
     breaks <- xLabels
   }
-  
+
   ggplot2::ggplot(df_long) +
     ggplot2::geom_tile(ggplot2::aes(x = .data$x, y = .data$y, fill = .data$value)) +
     ggplot2::scale_x_discrete(labels = breaks, breaks = breaks) +
@@ -40,18 +40,18 @@ makeHeatMap <- function(df, xTicksNum = 10){
 #'
 #' @param EI EpileptogenicIndex object from \code{computeEpileptogenicIndex}
 #' @param sozIndex Integer or string. A group of electrodes to mark as in the Seizure Onset Zone (SOZ)
-#' 
+#'
 #' @return A ggplot object
 #'
 #' @examples
-#' 
+#'
 #' data("pt01EcoG")
-#' 
+#'
 #' ## sozIndex is the index of the electrodes we assume are in the SOZ
 #' sozIndex <- attr(pt01EcoG, "sozIndex")
 #' ## precomputed Epileptogenic Index object
 #' data("pt01EI")
-#' 
+#'
 #' ## plot the mean power heatmap
 #' plotER<-plotERHeatmap(ei=pt01EI,sozIndex=sozIndex)
 #' plotER
@@ -65,17 +65,17 @@ plotERHeatmap <- function(
   ERMat <- ei$energyRatio
   elecNum <- nrow(ERMat)
   windowNum <- ncol(ERMat)
-  
+
   elecNames <- ei$electrodes
   sozIndex <- checkIndex(sozIndex, elecNames)
-  
+
   group1 <- sozIndex
   group2 <- setdiff(seq_len(elecNum), sozIndex)
-  
+
   elecColor <- rep("blue", elecNum)
   elecColor[seq_along(group2)] <- "black"
-  
-  startTime <- ei$startTimes
+
+  startTime <- round(ei$startTimes,1)
   if (is.null(startTime)) {
     xlabel <- "Time Index"
     stimes <- seq_len(windowNum)
@@ -83,15 +83,15 @@ plotERHeatmap <- function(
     xlabel <- "Time (s)"
     stimes <- startTime
   }
-  
+
   rownames(ERMat) <- ei$electrodes
   colnames(ERMat) <- stimes
-  
+
   ## prepare the data.frame for visualization
   allIndex <- c(group1, group2)
   df <- as.data.frame(ERMat[allIndex, ])
-  
-  
+
+
   makeHeatMap(df) +
     ggplot2::labs(x = xlabel, y = "Electrode", size = 2) +
     ggplot2::theme(
@@ -106,11 +106,11 @@ plotERHeatmap <- function(
 #'
 #' @examples
 #' data("pt01EcoG")
-#' 
+#'
 #' ## Visualize a subject of electrodes
 #' sozIndex <- attr(pt01EcoG, "sozIndex")
 #' display <- c(sozIndex, 77:80)
-#' 
+#'
 #' epoch <- Epoch(pt01EcoG)
 #' visuIEEGData(epoch = epoch[display, ])
 #' @export
@@ -118,16 +118,16 @@ visuIEEGData <- function(epoch) {
   if (is(epoch, "matrix")){
     epoch <- Epoch(epoch)
   }
-  
+
   gaps <- 2
-  
+
   elecNames <- epoch$electrodes
   data <- epoch$data
   elecNum <- nrow(data)
   timesNum <- ncol(data)
-  
+
   plotData <- standardizeIEEG(data)
-  
+
   times <- epoch$times
   if (is.null(times)) {
     xlabel <- "Time Index"
@@ -136,27 +136,27 @@ visuIEEGData <- function(epoch) {
     xlabel <- "Time (s)"
     timeTicks <- times
   }
-  
+
   plotData <- apply(plotData, 1, function(x) x - mean(x))
   plotData <- as.data.frame(plotData)
   plotData$timeTicks <- timeTicks
   breakplot <- (seq_len(elecNum) - 1) * gaps
-  
+
   elecNamesReversed <- rev(elecNames)
-  
+
   ## add gaps between electrodes
   for (i in seq_along(elecNamesReversed)) {
     elec <- elecNamesReversed[i]
     plotData[[elec]] <- plotData[[elec]] + (i-1) * gaps
   }
-  
-  
+
+
   p <- ggplot2::ggplot(data = plotData)
   for (i in seq_along(elecNamesReversed)) {
     elec <- elecNamesReversed[i]
     p <- p + ggplot2::geom_line(ggplot2::aes(x = .data$timeTicks, y = .data[[elec]]))
   }
-  
+
   p +
     ggplot2::labs(x = xlabel, y = "Electrode", size = 2) +
     ggplot2::scale_y_continuous(labels = elecNamesReversed, breaks = breakplot)
